28b085665f77b552c68464641a1afca98de03796
Luke <lukewhrit@pm.me>
2023-01-08 21:56:51 -0500

    feat: authenticate method and makeRequest helper function

--- eve/client.go
+++ eve/client.go
@@ -1,15 +1,53 @@
 package eve
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // Authenticate uses an email and password to login via Eve and returns a token on success or error on failure
 func Authenticate(email string, password string) (string, error) {
-	return "", nil
+	payload, err := encodeJSON(map[string]string{
+		"email":    email,
+		"password": password,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	r, err := hc.Post("http://10.10.9.4:3000/login", "application/json; charset=utf-8", &payload)
+
+	if err != nil {
+		return "", err
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	var marshalledJson map[string]string
+	if err := json.Unmarshal(body, &marshalledJson); err != nil {
+		return "", err
+	}
+
+	return marshalledJson["token"], nil
 }
 
 // Equivalent to the Authenticate function but panics on errors
 func MustAuthenticate(email string, password string) string {
-	return ""
+	str, err := Authenticate(email, password)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return str
 }
 
 type Client struct {
-	Token string
+	Instance string
+	Token    string
 }
--- eve/helpers.go
+++ eve/helpers.go
@@ -1,1 +1,75 @@
 package eve
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var hc = http.Client{
+	Timeout: time.Duration(1) * time.Second,
+}
+
+func unmarshalJSON(f io.Reader) (map[string]interface{}, error) {
+	body, err := io.ReadAll(f)
+
+	if err != nil {
+		return map[string]interface{}{}, err
+	}
+
+	var marshalledJson map[string]interface{}
+	if err := json.Unmarshal(body, &marshalledJson); err != nil {
+		return map[string]interface{}{}, err
+	}
+
+	return marshalledJson, nil
+}
+
+func encodeJSON(f any) (bytes.Buffer, error) {
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(f)
+
+	if err != nil {
+		return buf, err
+	}
+
+	return buf, nil
+}
+
+func (c *Client) makeRequest(route string, method string, payload any) (*http.Response, map[string]interface{}, error) {
+	p, err := encodeJSON(payload)
+
+	if err != nil {
+		return &http.Response{}, map[string]interface{}{}, err
+	}
+
+	req, err := http.NewRequest(
+		method,
+		fmt.Sprintf("%s/%s", c.Instance, route),
+		&p,
+	)
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Token))
+
+	if err != nil {
+		return &http.Response{}, map[string]interface{}{}, err
+	}
+
+	resp, err := hc.Do(req)
+
+	if err != nil {
+		return &http.Response{}, map[string]interface{}{}, err
+	}
+
+	body, err := unmarshalJSON(resp.Body)
+
+	if err != nil {
+		return &http.Response{}, map[string]interface{}{}, err
+	}
+
+	return resp, body, nil
+}
